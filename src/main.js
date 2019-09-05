bcba01eff1d53f2578e0123b1feeadd5ce77ec5e
wlissis <wlissisrds@gmail.com>
2019-09-05 19:41:19 -0300

    Commit Axios and Async/await

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
-const soma = ((a ,b) =>  a + b); // arrow function
+import { rejects } from "assert";
+import { resolve } from "url";
+import axios from 'axios';
 
-console.log(soma(1,7));
+class Api {
+    static async getUserInfor(username){
+        try {
+            const response = await axios.get(`https://api.github.com/users/${username}`); //metodo get vai retornar meu githube
+        
+            console.log(response);
+        } catch (error) {
+        
+        console.warn('Erro na API')
+
+        }
+    }            
+}
+
+Api.getUserInfor('wlissis');
